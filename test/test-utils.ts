9ac41e932f1ebaef39ae1d6866c680c6c742b0fb
Brad Reed 😎 <brad@bradreed.co.uk>
2022-01-02 18:18:31 +0000

    use unit test for deleteOne - others to follow

---
+++ test/test-utils.ts
@@ -0,0 +1,16 @@
+import stripAnsi from "strip-ansi";
+import { WrappedCollection } from "../src/types";
+
+export function mockCollection(isDry: boolean, mockedDocumentCount: number) {
+  return {
+    __migmong_options: {
+      dry: isDry,
+    },
+    __migmong_log: jest.fn(),
+    countDocuments: jest.fn().mockResolvedValueOnce(mockedDocumentCount),
+  } as unknown as jest.Mocked<WrappedCollection>;
+}
+
+export function stripLogLines(lines: (string | undefined)[]) {
+  return lines.map((arg) => (typeof arg === "string" ? stripAnsi(arg.replace(/\n/g, "")) : arg));
+}
