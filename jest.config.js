7c5387a91ed34454ec71c926021e5440922d4523
Brad Reed 😎 <brad@bradreed.co.uk>
2022-01-13 20:48:54 +0000

    more ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,5 @@ export default {
     "^.+\\.(t|j)s$": "esbuild-jest",
   },
   transformIgnorePatterns: [`node_modules/(?!${esModules})`],
-  testPathIgnorePatterns: ["<rootDir>/dist/"],
+  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/integration/", "<rootDir>/test"],
 };
